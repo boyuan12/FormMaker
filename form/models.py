b9c99bf23c77c2a7c0521a4577da1ed263cd25b6
Boyuan Liu <boyuanliu6@yahoo.com>
2020-11-17 15:30:27 -0800

    added new migrations file

--- form/models.py
+++ form/models.py
@@ -5,36 +5,35 @@ import uuid
 class InputField(models.Model):
     order = models.IntegerField()
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     label = models.CharField(max_length=100)
-    form_id = models.UUIDField()
+    form_id = models.IntegerField()
 
 class Form(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    # id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     name = models.CharField(max_length=100)
     user_id = models.IntegerField()
 
 class Response(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    # id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     user_id = models.IntegerField()
-    form_id = models.UUIDField()
+    form_id = models.IntegerField()
 
 class ResponseQuestion(models.Model):
-    question_id = models.UUIDField()
+    question_id = models.IntegerField()
     response = models.CharField(max_length=255)
-    response_id = models.UUIDField()
+    response_id = models.IntegerField()
 
 class MultipleChoiceField(models.Model):
     order = models.IntegerField()
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    # id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     label = models.CharField(max_length=100)
-    form_id = models.UUIDField()
+    form_id = models.IntegerField()
 
 class MultipleChoiceOption(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    # id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     label = models.CharField(max_length=100)
-    question_id = models.UUIDField()
+    question_id = models.IntegerField()
