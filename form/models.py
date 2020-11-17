12fb53d67264555c07139d36e904816755e9721c
Boyuan Liu <boyuanliu6@yahoo.com>
2020-11-17 15:26:47 -0800

    added new migrations files

--- form/models.py
+++ form/models.py
@@ -4,17 +4,20 @@ import uuid
 # Create your models here.
 class InputField(models.Model):
     order = models.IntegerField()
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     label = models.CharField(max_length=100)
     form_id = models.UUIDField()
 
 class Form(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     name = models.CharField(max_length=100)
     user_id = models.IntegerField()
 
 class Response(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     user_id = models.IntegerField()
     form_id = models.UUIDField()
 
@@ -25,11 +28,13 @@ class ResponseQuestion(models.Model):
 
 class MultipleChoiceField(models.Model):
     order = models.IntegerField()
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     label = models.CharField(max_length=100)
     form_id = models.UUIDField()
 
 class MultipleChoiceOption(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     label = models.CharField(max_length=100)
     question_id = models.UUIDField()
